b81b4046a48792125ddb97db952e4b48507b5e6a
foleyemma <follyemma03@gmail.com>
2024-01-26 22:45:10 +0100

    ok

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const scrollToTopButton = document.querySelector ('#scroll-to-top')
   origin: 'top',
   distance: '80px',
   duration: 3000,
-  viewFactor: 0,
+  viewFactor: 0.1,
   reset: true
 })
 
